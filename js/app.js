e56f46d1a695fc9f68540cb593d5121c0303074d
KamoKG <kamotsipa.gmail.com>
2017-11-08 09:32:53 +0200

    Blue Magic

---
+++ js/app.js
@@ -0,0 +1,39 @@
+﻿'use strict';
+
+$("#close").click(function() {
+	$(".contactRow").slideUp();
+});
+
+function showForm() {
+	$(".contactRow").slideToggle();
+}
+
+function closeForm() {
+	$(".contactRow").slideUp();
+}
+
+$(document).ready(function() {       
+   var scroll_start = 0;
+   var startchange = $('#fh5co-hero');
+   var offset = startchange.offset();
+    if (startchange.length){
+  		$(document).scroll(function() { 
+      	scroll_start = $(this).scrollTop();
+     	if(scroll_start > offset.top) {
+         	 $("#fix").css('background-color', '#333333');
+       	}
+
+        else {
+          $('#fix').css('background-color', 'transparent');
+      }
+   });
+    }
+});
+
+$("#formDiv").click(function(e) {
+   e.preventDefault();
+   var section = $(this).attr("href");
+   $("html body").animate(
+     {scrollTop: $(section).offset().scrollTop
+     });
+});
